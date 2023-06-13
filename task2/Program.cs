d46c7a3387ea0398e228594fd8021a7db602ed76
lesya_popova <lspopova99@gmail.com>
2023-06-14 07:55:18 +0800

    added task 2

---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Enter first number ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if(num1 > num2)
+{
+    Console.Write($"First number is bigger than second number, max = {num1}, min = {num2}");
+}
+if(num2 > num1)
+{
+    Console.Write($"Second number is bigger than first number, max = {num2}, min = {num1}");
+}
+else Console.Write("Numbers are equals");
+
