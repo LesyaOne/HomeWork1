d94cd2d6f2cc0047d95a79b3524512b7bd3fb4dc
lesya_popova <lspopova99@gmail.com>
2023-06-14 08:51:58 +0800

    added task6

---
+++ task6/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number % 2 == 0)
+{
+    Console.Write($"Number {number} is even");
+}
+else Console.WriteLine($"Number {number} is odd");
