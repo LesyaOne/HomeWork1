fe9c2a7c7ccaa5f1a9628d91db0383576cdb8549
lesya_popova <lspopova99@gmail.com>
2023-06-14 09:31:37 +0800

    added task8

---
+++ task8/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number > 1)
+{
+    int temp = 2;
+    Console.Write("Even numbers: ");
+    while(temp<=number)
+    {
+        Console.Write($"{temp}, ");
+        temp = temp + 2;
+    }
+}
+if(number < 1)
+{
+    int temp = 0;
+    Console.Write("Even numbers: ");
+    while(temp>=number)
+    {
+        Console.Write($"{temp}, ");
+        temp = temp - 2;
+    }
+}
+if(number==1) Console.Write("Your number is 1, ERROR!");
